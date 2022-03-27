31642613bd897407fa4d635980688727d5122b37
Florent Mertens <mertens.florent00@gmail.com>
2022-03-27 16:37:02 +0200

    Create repository

---
+++ main.js
@@ -0,0 +1,25 @@
+// var canvas = document.getElementById("canvas");
+// var image = new Image();
+
+// image.onload = function () {
+//   var ctx = canvas.getContext("2d");
+//   if (this.width >= window.innerWidth) {
+//     var w = (90 * window.innerWidth) / 100;
+//     var h = (50 * window.innerHeight) / 100;
+//   } else {
+//     var w = this.width;
+//     var h = this.height;
+//   }
+//   canvas.width = w;
+//   canvas.height = h;
+//   ctx.drawImage(image, 0, 0, w, h);
+// };
+
+// image.src = "images/florent.jpg";
+
+// var inputText = document.getElementById("response");
+// var submit = document.getElementById("submit");
+
+// submit.addEventListener("click", () => {
+//   console.log(inputText.value);
+// });
